b48d8e39eba09e0d6bcc5b1f952a0bfc0d22b7aa
Simon Korondi <sjkorondi@gmail.com>
2025-11-17 12:45:22 -0600

    templates and views

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import click
 from flask import current_app, g
 
-
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -14,7 +13,6 @@ def get_db():
         g.db.row_factory = sqlite3.Row
 
     return g.db
-
 
 def close_db(e=None):
     db = g.pop('db', None)
@@ -28,13 +26,11 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-
 @click.command('init-db')
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
-
 
 sqlite3.register_converter(
     "timestamp", lambda v: datetime.fromisoformat(v.decode())
