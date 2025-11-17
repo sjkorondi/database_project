32b36ae9242601669a4b224cda2a5163302efbac
Simon Korondi <sjkorondi@gmail.com>
2025-11-17 13:14:49 -0600

    finished tutorial

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -22,10 +22,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
-    def index():
-        return 'Welcome to the Flask App!'
-
     # a simple page that says hello
     @app.route('/hello')
     def hello():
@@ -36,6 +32,10 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
